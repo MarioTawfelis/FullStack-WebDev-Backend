16133ab4763c32044dddc02e1d6cd40b9f264f46
Mario <mario.tawfelis@outlook.com>
2023-10-05 18:16:07 +0100

    Refactored app into modules to follow best practicies

---
+++ part4/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.set('strictQuery', false)
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MonogDB')
+  })
+  .catch(error => logger.error('error connecting to MonogoDB:', error.message))
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
