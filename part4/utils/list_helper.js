dff839c9e53b1f2071b234452ee8fba285cacb8c
Mario <mario.tawfelis@outlook.com>
2023-10-14 14:25:56 +0100

    Testing favorite blog function

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,7 +8,23 @@ const totalLikes = (blogs) => {
     : blogs.reduce((sum, post) => sum + post.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  const mostLiked = blogs.reduce((curr, prev) => {
+    return curr.likes > prev.likes ? curr : prev
+  })
+
+  return {
+    title: mostLiked.title,
+    author: mostLiked.author,
+    url: mostLiked.url,
+    likes: mostLiked.likes
+  }
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
