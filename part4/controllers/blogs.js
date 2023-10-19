f5a0ee6258f3c64eb761677a88b865075101191c
Mario <mario.tawfelis@outlook.com>
2023-10-19 14:17:37 +0100

    More backend testing

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,22 +1,55 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+
+  response.json(blogs)
 })
 
-blogRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body)
+blogRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0
+  })
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog.toJSON())
+
+})
+
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const blog = request.body
+  const id = request.params.id
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blog, {
+    new: true,
+  }).populate('user', { username: 1, name: 1 })
+
+  updatedBlog
+    ? response.status(200).json(updatedBlog.toJSON())
+    : response.status(404).end()
 })
 
 module.exports = blogRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,26 +1,103 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
 const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
-test('blogs are returned as json', async () => {
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('blogs have id property named id instead of _id', async () => {
+    const response = await api.get('/api/blogs')
+
+    const ids = response.body.map((blog) => blog.id)
+
+    for (const id of ids) {
+      expect(id).toBeDefined()
+    }
+  })
+})
+
+test('a valid blog item can be added', async () => {
+  const newBlog = {
+    title: 'Some title',
+    author: 'Some author',
+    url: 'https://reactpatterns.com/',
+    likes: 3
+  }
+
   await api
-    .get('/api/blogs/')
-    .expect(200)
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
     .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsAtEnd.map(r => r.title)
+  expect(titles).toContain(
+    'Some title'
+  )
 })
 
-test('there are two blogs', async () => {
-  const response = await api.get('/api/blogs')
+test('likes property defaults to 0 if missing', async () => {
+  const newBlog = {
+    title: 'Missing like',
+    author: 'Liker',
+    url: 'https://www.example.com',
+  }
 
-  expect(response.body).toHaveLength(2)
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  expect(blogsAtEnd[blogsAtEnd.length - 1].likes).toBe(0)
 })
 
-test('the first blog has 4 likes', async () => {
+test('backend responds with status 400 if title and url are missing', async () => {
+  const newBlog = {
+    likes: 1,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+}, 100000)
+
+test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs')
 
-  expect(response.body[0].content).toBe(4)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+test('the first blog has 7 likes', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].likes).toBe(7)
 })
 
 afterAll(async () => {
