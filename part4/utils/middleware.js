fbb5f386b5ea15c3c3de832af23d1d225cb404cc
Mario <mario.tawfelis@outlook.com>
2023-11-01 13:37:26 +0000

    Moved the tokenExtractor to middleware

--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -10,25 +10,38 @@ const requestLogger = (request, response, next) => {
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
+
 }
 
 const errorHandler = (error, request, response, next) => {
   logger.error(error)
 
   if (error.name === 'CastError') {
-    response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message })
   } else if (error.name === 'JsonWebTokenError') {
-    response.status(401).json({ error: error.message })
+    return response.status(401).json({ error: 'invalud token' })
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' })
   }
 
   // If not, pass error to the defaul Express error handler
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.substring(7)
+  }
+
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
